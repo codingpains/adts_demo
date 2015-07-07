3d38e7a35126778fa8d55b0182530b1aa0f925b1
Gus Ortiz <guspidey@gmail.com>
2015-07-06 22:45:43 -0500

    Change pm2 app name

--- deploy/deploy_webserver.sh
+++ deploy/deploy_webserver.sh
@@ -14,8 +14,8 @@ npm install
 
 # Kill previous workers
 echo "-- Stop and remove all existing instances"
-pm2 stop web-server
-pm2 delete web-server
+pm2 stop adt-web-server
+pm2 delete adt-web-server
 
 # Start new workers
 echo "-- Start new insances: pm2 start ./deploy/pm2-webserver.json --env production"
